e929e8e89b167a4140188860dc29885332ea0dee
Unnikrishnan Patel <patel.unnikrishnan@gmail.com>
2016-04-26 18:42:02 +0530

    Demo and full build flavours added

--- app/src/full/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
+++ app/src/full/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
@@ -1,5 +1,4 @@
 package com.udacity.gradle.builditbigger;
-
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,6 +7,7 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.udacity.gradle.builditbigger.R;
 
 
 /**
@@ -22,16 +22,7 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
+        getActivity().setTitle(getActivity().getTitle()+" PRO");
         return root;
     }
 }
